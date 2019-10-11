163a36007f1426d3d324b1eb226da4101b1fca3c
Or Goldfus <orgoldfus@Ors-MacBook-Pro-2.local>
2019-10-11 13:55:39 +0300

    Add js-sql-parser

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-exports.parse = function (sqlQuery) {
+const sqlParser = require('js-sql-parser')
+
+function parse (sqlQuery) {
+  const ast = sqlParser.parse(sqlQuery)
+  console.log(JSON.stringify(ast))
   return {}
 }
+
+parse("SELECT * from foo where name='Marco'")
+
+exports.parse = parse
